31d0fa006b0d7efd2e5a86172fe39800b96165ac
Thomas Schüttler <tschuettler@oxfam.de>
2021-02-25 16:49:14 +0100

    Apply civilint

--- CRM/Gdpr/Page/AJAX.php
+++ CRM/Gdpr/Page/AJAX.php
@@ -24,7 +24,7 @@
  | GNU Affero General Public License or the licensing of CiviCRM,     |
  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
-*/
+ */
 
 /**
  *
@@ -40,14 +40,15 @@ require_once 'CRM/Utils/Type.php';
  *  Class used to get historic addresses
  */
 class CRM_Gdpr_Page_AJAX {
+
   /**
    * Function to get address log for a contact
    */
-  public static function get_address_logs($contactId){
-    
-    $aGetMemberships =array();
+  public static function get_address_logs($contactId) {
 
-    if(empty($contactId)){
+    $aGetMemberships = array();
+
+    if (empty($contactId)) {
       return $aGetMemberships;
     }
 
@@ -65,37 +66,36 @@ FROM {$logging_db}.log_civicrm_address as lca
 LEFT JOIN civicrm_location_type as lt ON ( lca.location_type_id = lt.id )
 WHERE lca.contact_id = %1
 ORDER BY lca.log_date DESC";
-      $sqlParams = array( 
-        1 => array( $contactId, 'Integer')
+    $sqlParams = array(
+      1 => array($contactId, 'Integer'),
+    );
+    $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+    while ($dao->fetch()) {
+      $country = empty($dao->country_id) ? NULL : CRM_Core_PseudoConstant::country($dao->country_id);
+      $county = empty($dao->county_id) ? NULL : CRM_Core_PseudoConstant::county($dao->county_id);
+      $aGetMemberships[] = array(
+        'street'        => $dao->street_address,
+        'location_type' => $dao->lt_name,
+        'postal_code'   => $dao->postal_code,
+        'country'       => $country,
+        'line1'         => $dao->supplemental_address_1,
+        'line2'         => $dao->supplemental_address_2,
+        'line3'         => $dao->supplemental_address_3,
+        'city'          => $dao->city,
+        'country'       => $country,
+        'county'        => $county,
+        'log_action'    => $dao->log_action,
+        'log_date'      => CRM_Utils_Date::customFormat($dao->log_date, '%d-%m-%Y'),
       );
-      $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
-      while($dao->fetch()){
-          $country  = empty($dao->country_id) ? Null  : CRM_Core_PseudoConstant::country( $dao->country_id );
-          $county   = empty($dao->county_id)  ? Null  : CRM_Core_PseudoConstant::county( $dao->county_id );
-          $aGetMemberships[] = array(
-            'street'        => $dao->street_address,
-            'location_type' => $dao->lt_name,
-            'postal_code'   => $dao->postal_code,
-            'country'       => $country,
-            'line1'         => $dao->supplemental_address_1,
-            'line2'         => $dao->supplemental_address_2,
-            'line3'         => $dao->supplemental_address_3,
-            'city'          => $dao->city,
-            'country'       => $country,
-            'county'        => $county,
-            'log_action'    => $dao->log_action,
-            'log_date'      => CRM_Utils_Date::customFormat($dao->log_date , '%d-%m-%Y')
-          );
-      }
-    //}
+    }
     return $aGetMemberships;
   }
-  
-  public static function get_address_history_table( $data ){
-    if(empty($data)){
-      return null;
+
+  public static function get_address_history_table($data) {
+    if (empty($data)) {
+      return NULL;
     }
-    $table  = <<< TABLE
+    $table = <<< TABLE
       <table id="custom_address_history">
         <thead>
           <tr>
@@ -119,58 +119,54 @@ ORDER BY lca.log_date DESC";
             </th>
           </tr>
         </thead>
-    <tbody>
+        <tbody>
 TABLE;
-    
-        
-        foreach( $data as $row ){
-         $table  .= "<tr> ";
-         $table  .= "<td valign='top'> ".$row['log_date']."</td> ";
-         $table  .= "<td valign='top'> ".$row['log_action']."</td> ";
-         $table  .= "<td valign='top'> ".$row['location_type']."</td> ";
-         $table  .= "<td valign='top'> ".$row['street'];
-         if(!empty( $row['line1'])){
-           $table .= "<br>".$row['line1'];
-         }
-         if(!empty( $row['line2'])){
-           $table .= "<br>".$row['line2'];
-         }
-         if(!empty( $row['line3'])){
-           $table .= "<br>".$row['line3'];
-         }
-         if(!empty( $row['city'])){
-           $table .= "<br>".$row['city'];
-         }
-         $table  .= "</td>";
-         $table  .= "<td valign='top'> ".$row['postal_code']."</td> ";
-         $table  .= "<td valign='top'> ".$row['country']."</td> ";
-         $table  .= "</tr> ";
-         
-        }
-     
+
+    foreach ($data as $row) {
+      $table .= "<tr> ";
+      $table .= "<td valign='top'> " . $row['log_date'] . "</td> ";
+      $table .= "<td valign='top'> " . $row['log_action'] . "</td> ";
+      $table .= "<td valign='top'> " . $row['location_type'] . "</td> ";
+      $table .= "<td valign='top'> " . $row['street'];
+      if (!empty($row['line1'])) {
+        $table .= "<br>" . $row['line1'];
+      }
+      if (!empty($row['line2'])) {
+        $table .= "<br>" . $row['line2'];
+      }
+      if (!empty($row['line3'])) {
+        $table .= "<br>" . $row['line3'];
+      }
+      if (!empty($row['city'])) {
+        $table .= "<br>" . $row['city'];
+      }
+      $table .= "</td>";
+      $table .= "<td valign='top'> " . $row['postal_code'] . "</td> ";
+      $table .= "<td valign='top'> " . $row['country'] . "</td> ";
+      $table .= "</tr> ";
+    }
+
     $table .= <<<TABLE
-          </tbody>
-        
-        </table>
+        </tbody>
+      </table>
 TABLE;
-    
-    
+
     return $table;
   }
 
-  static function getAddressHistory(){
+  public static function getAddressHistory() {
     $iContactId = $_POST['contactId'];
-    if(!empty($iContactId)){
-      $aGetAddressHistory = self::get_address_logs( $iContactId );
-      $table = self::get_address_history_table( $aGetAddressHistory );
+    if (!empty($iContactId)) {
+      $aGetAddressHistory = self::get_address_logs($iContactId);
+      $table = self::get_address_history_table($aGetAddressHistory);
     }
-    
+
     $countHistory = count($aGetAddressHistory);
     //$return['table'] = $table;
     //echo json_encode($return);
     $return = "";
-    if(!empty( $table ) && !empty($countHistory)){
-      $return = $countHistory.'|'.$table;
+    if (!empty($table) && !empty($countHistory)) {
+      $return = $countHistory . '|' . $table;
     }
     echo $return;
     exit;
@@ -225,20 +221,20 @@ TABLE;
   /**
    * To handle the comms preference submission from Thank you page Event/Contribution page.
    */
-  static function commPreferenceSubmission(){
+  public static function commPreferenceSubmission() {
     $iContactId = $_POST['contactId'];
     $checksum = $_POST['contact_cs'];
     $submittedValues = $_POST['preference'];
 
     if (empty($iContactId) || !CRM_Contact_BAO_Contact_Utils::validChecksum($iContactId, $checksum)) {
-      echo "Failed to Update communication preferences." . $checksum ;
+      echo "Failed to Update communication preferences." . $checksum;
       CRM_Utils_System::civiExit();
     }
 
     //Update preferences
     CRM_Gdpr_CommunicationsPreferences_Utils::updateCommsPrefByFormValues($iContactId, $submittedValues);
 
-    //Create comms preference activity 
+    //Create comms preference activity
     CRM_Gdpr_CommunicationsPreferences_Utils::createCommsPrefActivity($iContactId, $submittedValues);
 
     //Get completion msg from settings
@@ -254,10 +250,11 @@ TABLE;
     CRM_Utils_System::civiExit();
   }
 
-  static function reRunInstallationCustomData() {
+  public static function reRunInstallationCustomData() {
     CRM_Gdpr_Utils::reRunInstallationCustomXML();
 
     echo "Custom Data XMl processed successfully";
     CRM_Utils_System::civiExit();
   }
+
 }
